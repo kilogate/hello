cf7e98acf8ea1e0925dfa5b07c5212f858000f0c
fengquanwei <fengquanwei@bytedance.com>
2022-06-03 10:28:57 +0800

    helloworld and echo

---
+++ gopl/ch1/echo3.go
@@ -0,0 +1,12 @@
+// go run gopl/ch1/echo3.go 1 2 3 4
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
---
+++ gopl/ch1/helloworld.go
@@ -0,0 +1,8 @@
+// go run gopl/ch1/helloworld.go
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello, world")
+}
