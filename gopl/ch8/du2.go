c1bc2b5b800b7f65a42c75cfdf738ca5c23ef180
kilogate <271806949@qq.com>
2022-09-12 15:42:48 +0800

    du

--- gopl/ch8/du2.go
+++ gopl/ch8/du2.go
@@ -6,13 +6,10 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 )
 
-var verbose = flag.Bool("v", false, "show verbose progress messages")
-
-// main
-// go run gopl/ch8/du1.go -v ~/
 func main() {
 	// Determine the initial directories.
 	flag.Parse()
@@ -23,21 +20,22 @@ func main() {
 
 	// Traverse the file tree.
 	fileSizes := make(chan int64)
+	var wg sync.WaitGroup
 	go func() {
 		for _, root := range roots {
-			walkDir(root, fileSizes)
+			wg.Add(1)
+			go walkDir2(root, &wg, fileSizes)
 		}
+	}()
+	go func() {
+		wg.Wait()
 		close(fileSizes)
 	}()
 
 	// Print the results periodically.
-	var tick <-chan time.Time
-	if *verbose {
-		tick = time.Tick(100 * time.Millisecond)
-	}
+	tick := time.Tick(100 * time.Millisecond)
 
 	var nFiles, nBytes int64
-
 loop:
 	for {
 		select {
@@ -48,20 +46,22 @@ loop:
 			nFiles++
 			nBytes += size
 		case <-tick:
-			printDiskUsage(nFiles, nBytes)
+			printDiskUsage2(nFiles, nBytes)
 		}
 	}
-	printDiskUsage(nFiles, nBytes) // final totals
+	printDiskUsage2(nFiles, nBytes) // final totals
 	fmt.Println()
 }
 
 // walkDir recursively walks the file tree rooted at dir
 // and sends the size of each found file on fileSizes.
-func walkDir(dir string, fileSizes chan<- int64) {
-	for _, entry := range dirEntries(dir) {
+func walkDir2(dir string, wg *sync.WaitGroup, fileSizes chan<- int64) {
+	defer wg.Done()
+	for _, entry := range dirEntries2(dir) {
 		if entry.IsDir() {
+			wg.Add(1)
 			subDir := filepath.Join(dir, entry.Name())
-			walkDir(subDir, fileSizes)
+			go walkDir2(subDir, wg, fileSizes)
 		} else {
 			fileSizes <- entry.Size()
 		}
@@ -69,7 +69,7 @@ func walkDir(dir string, fileSizes chan<- int64) {
 }
 
 // dirEntries returns the entries of directory dir.
-func dirEntries(dir string) []os.FileInfo {
+func dirEntries2(dir string) []os.FileInfo {
 	entries, err := ioutil.ReadDir(dir)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "du1: %v\n", err)
@@ -78,6 +78,6 @@ func dirEntries(dir string) []os.FileInfo {
 	return entries
 }
 
-func printDiskUsage(nFiles, nBytes int64) {
+func printDiskUsage2(nFiles, nBytes int64) {
 	fmt.Printf("\r%d files  %.1f GB", nFiles, float64(nBytes)/1e9)
 }
