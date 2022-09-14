98842cdece380949f7bf28d472d49fc5ae7c0c89
kilogate <271806949@qq.com>
2022-09-14 21:17:19 +0800

    du3

--- gopl/ch8/du3/du3.go
+++ gopl/ch8/du3/du3.go
@@ -13,6 +13,8 @@ import (
 // sema is a counting semaphore for limiting concurrency in dirents.
 var sema = make(chan struct{}, 8)
 
+var done = make(chan struct{})
+
 // main go run gopl/ch8/du3/du3.go ~/
 func main() {
 	now := time.Now()
@@ -23,6 +25,12 @@ func main() {
 	if len(roots) == 0 {
 		roots = []string{"."}
 	}
+
+	// Cancel traversal when input is detected.
+	go func() {
+		os.Stdin.Read(make([]byte, 1)) // read a single byte
+		close(done)
+	}()
 
 	// Traverse the file tree.
 	fileSizes := make(chan int64)
@@ -53,6 +61,13 @@ loop:
 			nBytes += size
 		case <-tick:
 			printDiskUsage(nFiles, nBytes)
+		case <-done:
+			// Drain fileSizes to allow existing goroutines to finish.
+			for range fileSizes {
+				// Do nothing.
+			}
+			fmt.Println("任务被停止")
+			return
 		}
 	}
 	printDiskUsage(nFiles, nBytes) // final totals
@@ -64,10 +79,13 @@ loop:
 // and sends the size of each found file on fileSizes.
 func walkDir(dir string, wg *sync.WaitGroup, fileSizes chan<- int64) {
 	defer wg.Done()
+	if cancelled() {
+		return
+	}
 	for _, entry := range dirEntries(dir) {
 		if entry.IsDir() {
-			wg.Add(1)
 			subDir := filepath.Join(dir, entry.Name())
+			wg.Add(1)
 			go walkDir(subDir, wg, fileSizes)
 		} else {
 			fileSizes <- entry.Size()
@@ -77,7 +95,12 @@ func walkDir(dir string, wg *sync.WaitGroup, fileSizes chan<- int64) {
 
 // dirEntries returns the entries of directory dir.
 func dirEntries(dir string) []os.FileInfo {
-	sema <- struct{}{}        // acquire token
+	select {
+	case sema <- struct{}{}: // acquire token
+	case <-done:
+		return nil // cancelled
+	}
+
 	defer func() { <-sema }() // release token
 
 	entries, err := ioutil.ReadDir(dir)
@@ -90,4 +113,13 @@ func dirEntries(dir string) []os.FileInfo {
 
 func printDiskUsage(nFiles, nBytes int64) {
 	fmt.Printf("\r%d files  %.1f GB", nFiles, float64(nBytes)/1e9)
+}
+
+func cancelled() bool {
+	select {
+	case <-done:
+		return true
+	default:
+		return false
+	}
 }
