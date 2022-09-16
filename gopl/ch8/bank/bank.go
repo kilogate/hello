9ec42dbd2c5518e0a458e2801d0d355c73baba7d
kilogate <271806949@qq.com>
2022-09-16 11:51:01 +0800

    bank

---
+++ gopl/ch8/bank/bank.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var balance int
+
+func Deposit(amount int) {
+	balance = balance + amount
+}
+
+func Balance() int {
+	return balance
+}
+
+func main() {
+	fmt.Printf("main start, balance: %v\n", Balance())
+	var wg sync.WaitGroup
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 100; j++ {
+				Deposit(1)
+			}
+		}()
+	}
+
+	wg.Wait()
+	fmt.Printf("main end, balance: %v\n", Balance())
+}
