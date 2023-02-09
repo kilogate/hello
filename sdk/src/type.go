697a80c0475e7024b3cc7e63b73f485cc7f77578
kilogate <271806949@qq.com>
2023-02-09 15:25:34 +0800

    update

--- sdk/src/type.go
+++ sdk/src/type.go
@@ -9,7 +9,7 @@ func main() {
 
 func testMake() {
 	// 1 数组不用make可以直接使用
-	var strArray [1]string   // 长度为1元素为空的数组
+	var strArray [1]string   // 长度为1，元素为空的数组
 	fmt.Println(strArray[0]) // 可读
 	strArray[0] = "ABC"      // 可写
 
@@ -19,12 +19,12 @@ func testMake() {
 	//strSlice[0] = "ABC" // 写报错，runtime error: index out of range [0] with length 0
 
 	// nil切片支持直接append，append之后返回新切片
-	strSlice = append(strSlice, "a") // 长度为1容量为1元素为a的切片
+	strSlice = append(strSlice, "a") // 长度为1，容量为1，元素为"a"的切片
 	fmt.Println(strSlice[0])         // 下标大于等于长度的时候会报错，runtime error: index out of range [0] with length 0
 	strSlice[0] = "ABC"              // 下标大于等于长度的时候会报错，runtime error: index out of range [0] with length 0
 
 	// make创建指定长度和容量的切片
-	strSlice = make([]string, 3, 10) // 长度为3容量为10元素为空的切片
+	strSlice = make([]string, 3, 10) // 长度为3，容量为10，元素为空的切片
 	fmt.Println(strSlice[0])         // 下标大于等于长度的时候会报错，runtime error: index out of range [0] with length 0
 	strSlice[0] = "ABC"              // 下标大于等于长度的时候会报错，runtime error: index out of range [0] with length 0
 
