4ed036eb1c4759fc31081601e4d5230593ace6e8
kilogate <271806949@qq.com>
2023-02-16 20:43:14 +0800

    string

--- sdk/src/string.go
+++ sdk/src/string.go
@@ -11,8 +11,17 @@ func main() {
 	fmt.Println(utf8.RuneCountInString(s)) // 字符数：5
 
 	// 取的是字节，uint8 类型
-	u := s[0]
-	fmt.Println(string(u))
-	u2 := s[7]
-	fmt.Println(string(u2))
+	u := s[0]                          // uint8 类型，值为 233
+	u2 := s[7]                         // uint8 类型，值为 54
+	fmt.Println(string(u), string(u2)) // "é" "6"
+
+	// 取子串也是按字节取的
+	s2 := s[0:1] // �
+	s3 := s[0:2] // ��
+	s4 := s[0:3] // 飞
+	s5 := s[0:4] // 飞�
+	fmt.Println(s2, s3, s4, s5)
+
+	// 字符串不可修改
+	//s[7] = 7 // 编译报错：Cannot assign to s[7]
 }
