7ade4cdc5e10601c3deb065df323c6bf524f0463
kilogate <271806949@qq.com>
2023-02-20 10:52:07 +0800

    1

--- sdk/src/bytes/bytes.go
+++ sdk/src/bytes/bytes.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	testBytes()
+	testBuffer()
 }
 
 func testBytes() {
@@ -17,5 +18,17 @@ func testBytes() {
 	fmt.Println(bytes.HasSuffix([]byte{'a', 'b', 'c'}, []byte{'a', 'b'}))            // false
 	fmt.Println(bytes.Index([]byte{'a', 'b', 'c'}, []byte{'a', 'b'}))                // 0
 	fmt.Println(bytes.Join([][]byte{{'a'}, {'a'}, {'a'}}, []byte{','}))              // [97 44 97 44 97]
+}
 
+func testBuffer() {
+	var buf bytes.Buffer
+
+	buf.WriteByte('1')    // 只能写单字节的字符（单字节兼容UTF8编码，可以直接写入）
+	buf.WriteString(", ") // 写字符串（本身就是UTF8编码的）
+	buf.WriteRune('二')    // 写任意字符（使用UTF8编码后再写入）
+	buf.WriteString(", ")
+	fmt.Fprintf(&buf, "%d", 3) // 这里应该也有UTF8编码
+	s := buf.String()
+
+	fmt.Println(s)
 }
