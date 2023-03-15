4cebdd5dfa45f86c5822cbae557d3209edbe9b0b
kilogate <271806949@qq.com>
2023-03-15 19:17:06 +0800

    rsa

--- sdk/src/crypto/rsa/rsa.go
+++ sdk/src/crypto/rsa/rsa.go
@@ -28,7 +28,8 @@ func genKey() (*rsa.PrivateKey, *rsa.PublicKey) {
 }
 
 func RSAEncrypt(originData string, publicKey *rsa.PublicKey) string {
-	encryptBytes, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, publicKey, []byte(originData), nil)
+	originBytes := []byte(originData)
+	encryptBytes, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, publicKey, originBytes, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -37,7 +38,7 @@ func RSAEncrypt(originData string, publicKey *rsa.PublicKey) string {
 
 func RSADecrypt(encryptData string, privateKey *rsa.PrivateKey) string {
 	encryptBytes, _ := base64.StdEncoding.DecodeString(encryptData)
-	decryptBytes, err := privateKey.Decrypt(rand.Reader, encryptBytes, &rsa.OAEPOptions{Hash: crypto.SHA256})
+	decryptBytes, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, privateKey, encryptBytes, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -56,7 +57,6 @@ func RSASign(originData string, privateKey *rsa.PrivateKey) string {
 func RSAVerify(originData, sign string, publicKey *rsa.PublicKey) bool {
 	shaBytes := sha256.Sum256([]byte(originData))
 	signBytes, err := base64.StdEncoding.DecodeString(sign)
-
 	err = rsa.VerifyPSS(publicKey, crypto.SHA256, shaBytes[:], signBytes, nil)
 	if err != nil {
 		return false
