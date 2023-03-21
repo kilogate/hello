db99171d2bd33f7510f521306cbac3a8d2bee363
kilogate <271806949@qq.com>
2023-03-21 15:22:07 +0800

    bufio

--- sdk/src/bufio/bufio.go
+++ sdk/src/bufio/bufio.go
@@ -3,11 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"unicode"
 )
 
 func main() {
 	//testScanner()
+	testReader()
 }
 
 func testScanner() {
@@ -16,5 +19,25 @@ func testScanner() {
 	for scanner.Scan() {
 		text := scanner.Text()
 		fmt.Println(text)
+	}
+}
+
+func testReader() {
+	// 从控制台输入，通过 Ctrl + d 退出输入
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, n, err := in.ReadRune() // returns rune, nbytes, error
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Printf("ReadRune occurs err, err: %v\n", err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar && n == 1 {
+			fmt.Printf("ReadRune found invalid rune, r: %q\n", r)
+			continue
+		}
+		fmt.Printf("rune: %q, bytes: %d\n", r, n)
 	}
 }
