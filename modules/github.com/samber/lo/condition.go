72ece759f7a0ceb74ad22ce2abe00317de1b4385
kilogate <271806949@qq.com>
2023-03-31 11:58:45 +0800

    samber lo condition

--- modules/github.com/samber/lo/condition.go
+++ modules/github.com/samber/lo/condition.go
@@ -7,6 +7,22 @@ import (
 )
 
 func main() {
-	res := lo.Ternary(true, "IF", "ELSE")
-	fmt.Println(res)
+	// ternary
+	ternary := lo.Ternary(5 > 3, "if", "else")
+	ternaryF := lo.TernaryF(5 > 3, func() string { return "if" }, func() string { return "else" })
+	fmt.Println(ternary, ternaryF)
+
+	// if else
+	ifElse := lo.If(5 > 8, "if").ElseIf(6 > 8, "else if").Else("else")
+	ifElseF := lo.IfF(5 > 8, func() string { return "if" }).ElseIfF(6 > 8, func() string { return "else if" }).ElseF(func() string { return "else" })
+	fmt.Println(ifElse, ifElseF)
+
+	// switch
+	s := "F"
+	switchCase := lo.Switch[string, string](s).
+		Case("A", "is A").
+		Case("B", "is B").
+		CaseF("C", func() string { return "is C" }).
+		Default("not match")
+	fmt.Println(switchCase)
 }
